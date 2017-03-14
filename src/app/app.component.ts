a4dad2d5f872450f2344bc09f213c0dcc8915b43
Brian Hendel <brian.hendel@gmail.com>
2017-03-14 19:22:26 -0400

    Frontend completed through part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Angular2TokenService } from 'angular2-token';
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+
+  constructor(private authToken: Angular2TokenService) {
+    this.authToken.init(environment.token_auth_config);
+
+  }
 }
